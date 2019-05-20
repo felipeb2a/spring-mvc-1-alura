a0376435616c97cef2c33ac494ec2bdb1628e269
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-05-20 20:25:53 -0300

    aula 08 - Enviando email

--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -1,6 +1,11 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.concurrent.Callable;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -22,21 +28,37 @@ public class PagamentoController {
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@Autowired
+	  private MailSender sender;
+	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
-	public ModelAndView finalizar(RedirectAttributes model){
-		String uri = "http://book-payment.herokuapp.com/payment";
-		
-		try {
-			String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
-			model.addFlashAttribute("message", response);
-			System.out.println(response);
-			//método para tirar todos os livros do carrinho
-			this.carrinho.limpa();
-			return new ModelAndView("redirect:/");
-		} catch (HttpClientErrorException e) {
-			e.printStackTrace();
-			model.addFlashAttribute("message", "Valor maior que o permitido! Compra negada!");
-			return new ModelAndView("redirect:/");
-		}
+	public Callable<ModelAndView> finalizar(@AuthenticationPrincipal Usuario usuario, RedirectAttributes model){
+	    return () -> {
+	    String uri = "http://book-payment.herokuapp.com/payment";
+	        try {
+	        String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
+	      System.out.println(response);
+
+	      // envia email para o usuário        
+	      enviaEmailCompraProduto(usuario);   
+
+	      model.addFlashAttribute("sucesso", response);
+
+	            return new ModelAndView("redirect:/produtos");
+
+	        } catch (HttpClientErrorException e) {
+	            e.printStackTrace();
+	      model.addFlashAttribute("falha", "Valor maior que o permitido");
+	      return new ModelAndView("redirect:/produtos");
+	        }
+	    };
+	}
+	
+	private void enviaEmailCompraProduto(Usuario usuario) {
+	    SimpleMailMessage email = new SimpleMailMessage();
+	    email.setSubject("Compra finalizada com sucesso");
+	    email.setTo(usuario.getEmail());
+	    email.setText("Compra aprovada com sucesso no valor de " + carrinho.getTotal());
+	    email.setFrom("compras@casadocodigo.com.br");
 	}
 }
